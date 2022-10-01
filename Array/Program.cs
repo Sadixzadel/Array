f3d4c46010fdf5d79e62dd9ea346a9ca8ee46f9f
HP <HP@DESKTOP-EQ9MNTC>
2022-10-02 01:39:26 +0400

    Добавьте файлы проекта.

---
+++ Array/Program.cs
@@ -0,0 +1,13 @@
+﻿using Homework3;
+using System;
+
+int size = 11;
+
+var array = new MyArray(size);
+Console.WriteLine(array);
+
+array.Sort(size);
+Console.WriteLine(array);
+
+array.Reverse(size);
+Console.WriteLine(array);
